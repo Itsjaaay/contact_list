cb027b2b7d07e4b73327eaba4c8f6640358a3d66
Itsjaaay <jaquan.williams101@gmail.com>
2023-11-29 16:08:43 -0600

    jaysWRLD

--- command-line-crud-app/mycrudproject/mycrudapp/forms.py
+++
@@ -1,9 +0,0 @@
-# app/forms.py
-
-from django import forms
-from .models import Task
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['description', 'status']
--- command-line-crud-app/project/app/tests.py
+++ command-line-crud-app/project/app/tests.py
@@ -5,12 +5,12 @@ from .models import Model
 # Create your tests here.
 class TestModel_test_cases(TestCase):
     def test_Model_creation(self):
-        Model = Model(title="Pokemon")
+        Model = Model(title="dog")
         Model.save()
         self.assertEqual(Model.objects.count(), 1)
 
     def test_read_all_games(self):
-        Model = Model.objects.create(title="Pokemon")
+        Model = Model.objects.create(title="dog")
         Model.save()
 
         Models = Model.objects.all()
@@ -18,31 +18,31 @@ class TestModel_test_cases(TestCase):
         self.assertEqual(len(title), 1)
 
     def test_delete_Model(self):
-        D = Model(title="Pokemon")
+        D = Model(title="dog")
         D.save()
 
-        # title = [Model.title for Model in Models]
+        
 
         D.delete()
         Models = Model.objects.all()
         self.assertEqual(len(Models), 0)
 
     def test_update_Model(self):
-        Model = Model.objects.create(title="Pokemon")
+        Model = Model.objects.create(title="dog")
         Model.save()
-        Model.title = "Pokemon: Black"
+        Model.title = "dog: Black"
         Model.save()
 
-        self.assertEqual(Model.title, "Pokemon: Black")
+        self.assertEqual(Model.title, "dog: Black")
 
     def test_read_by_title(self):
-        Model = Model.objects.create(title="Pokemon")
+        Model = Model.objects.create(title="dog")
         Model.save()
-        Model_return = Model.objects.get(title="Pokemon")
-        self.assertEqual(Model_return.title, "Pokemon")
+        Model_return = Model.objects.get(title="dog")
+        self.assertEqual(Model_return.title, "dog")
 
     def test_filtered_search(self):
-        Model = Model.objects.create(title="Pokemon")
+        Model = Model.objects.create(title="dog")
         Model.save()
-        Model_return = Model.objects.filter(title="Pokemon")
-        self.assertEqual(Model_return[0].title, "Pokemon")
+        Model_return = Model.objects.filter(title="dog")
+        self.assertEqual(Model_return[0].title, "dog")
