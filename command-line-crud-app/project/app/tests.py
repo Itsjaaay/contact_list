d10736bea0eca5f3dfc984545a5a84887f82d92e
Itsjaaay <jaquan.williams101@gmail.com>
2023-11-30 11:19:31 -0600

    jaysWRLD

--- command-line-crud-app/project/app/tests.py
+++ command-line-crud-app/project/app/tests.py
@@ -5,13 +5,13 @@ from .models import Model
 # Create your tests here.
 class TestModel_test_cases(TestCase):
     def test_Model_creation(self):
-        Model = Model(title="dog")
-        Model.save()
+        Model1 = Model(title="dog")
+        Model1.save()
         self.assertEqual(Model.objects.count(), 1)
 
     def test_read_all_games(self):
-        Model = Model.objects.create(title="dog")
-        Model.save()
+        Model1 = Model.objects.create(title="dog")
+        Model1.save()
 
         Models = Model.objects.all()
         title = [Model.title for Model in Models]
@@ -28,21 +28,21 @@ class TestModel_test_cases(TestCase):
         self.assertEqual(len(Models), 0)
 
     def test_update_Model(self):
-        Model = Model.objects.create(title="dog")
-        Model.save()
-        Model.title = "dog: Black"
-        Model.save()
+        Model1 = Model.objects.create(title="dog")
+        Model1.save()
+        Model1.title = "dog: Black"
+        Model1.save()
 
-        self.assertEqual(Model.title, "dog: Black")
+        self.assertEqual(Model1.title, "dog: Black")
 
     def test_read_by_title(self):
-        Model = Model.objects.create(title="dog")
-        Model.save()
+        Model1 = Model.objects.create(title="dog")
+        Model1.save()
         Model_return = Model.objects.get(title="dog")
         self.assertEqual(Model_return.title, "dog")
 
     def test_filtered_search(self):
-        Model = Model.objects.create(title="dog")
-        Model.save()
+        Model1 = Model.objects.create(title="dog")
+        Model1.save()
         Model_return = Model.objects.filter(title="dog")
         self.assertEqual(Model_return[0].title, "dog")
